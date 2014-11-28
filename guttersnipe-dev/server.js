8097fefa9bd55374827c3d73adc01206a5684d65
mitchell <mitchell.verter@gmail.com>
2014-11-28 11:51:34 -0500

    MEAN stack is workinggit add -A!

--- guttersnipe-dev/server.js
+++ guttersnipe-dev/server.js
@@ -1,4 +1,7 @@
-var express = require('express');
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    logger = require('logger');
+
 
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
@@ -8,12 +11,12 @@ var app = express();
 app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
 
+app.use(bodyParser());
+//app.use(logger('dev'))
 app.use(express.static(__dirname + '/public'));
 
 app.get('*', function respondToHTTPRequest(req, res) {
-   // res.set('Content-Type', mime.lookup(x));
-   // res.setHeader('content-type', 'text/javascript');
-    res.render('index');
+    res.sendFile( __dirname + '/server/views'+'/index.html');
 });
 
 var port = 4444;
