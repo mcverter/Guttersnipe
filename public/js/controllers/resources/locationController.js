8bbf3cc2519881e88d2a4637b835b0f968c6baea
mitchell <mitchell.verter@gmail.com>
2014-11-29 21:46:30 -0500

    geocoding

---
+++ public/js/controllers/resources/locationController.js
@@ -0,0 +1,9 @@
+(function (angular, app, _) {
+    'use strict';
+
+    app.controller('LocationCtrl', ['$scope', '$log', 'geocodeService',
+        function ($scope, $log, geocodeService) {
+            var moo = {};
+        }]);
+
+}) (window.angular, window.guttersnipe, window._);
