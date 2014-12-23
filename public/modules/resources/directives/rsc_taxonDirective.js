1d58d4d30243a5b8be69e3693b342ba01bc7cb94
mcverter <mitchell.verter@gmail.com>
2014-12-23 13:16:36 -0500

    Victory over Scopes!  Taxon colors work

--- public/modules/resources/directives/rsc_taxonDirective.js
+++ public/modules/resources/directives/rsc_taxonDirective.js
@@ -2,78 +2,15 @@
   'use strict';
 
   app.directive('resourceTaxon', ['filePaths', function(filePaths) {
-      var templateUrl = filePaths.resources_create_wizard + 'rsc_Taxon.html',
-        selected = false,
-        hovered = false;
+      var templateUrl = filePaths.resources_create_wizard + 'rsc_Taxon.html';
 
       return {
         restrict: 'E',
         templateUrl: templateUrl,
-        scope: {
-          selected:'&',
-          hovered:'&'
-        },
+
         link: function (scope, element, attrs) {
           scope.name = attrs.name;
         }
-          /*
-           link: function (scope, element, attrs) {
-           Object.defineProperties(scope, {
-           name: {
-           enumerable: true,
-           value: attrs.name
-           },
-           selected: {
-
-                        get: function() {
-                          return selected;
-                        },
-                        set : function(value) {
-                          selected = value;
-                        },
-                        enumerable: true
-                      },
-                      hovered: {
-                        get: function() {
-                          return hovered;
-                        },
-                        set : function(value) {
-                          hovered = value;
-                        },
-                        enumerable: true
-                      },
-
-                      buttonClass: {
-                        value: function() {
-                          return buttonClass;
-                        },
-                        enumerable: true
-                      },
-                      changeHoverColor: {
-                        enumerable: true,
-                        value: function ($event) {
-                          $event.preventDefault();
-                          if (buttonClass === 'button-default') {
-                            buttonClass = 'button-warning';
-                            console.log("changing class to ", buttonClass)
-                          }
-                        }
-                      },
-                      changeClickColor: {
-                        enumerable: true,
-                        value: function ($event) {
-                          $event.preventDefault();
-                          if (buttonClass === 'button-success') {
-                            buttonClass = 'button-default';
-                            console.log("changing class to ", buttonClass)
-                          }
-                          else {
-                            buttonClass = 'button-success';
-                            console.log("changing class to ", buttonClass)
-                          }
-                        }}
-                    });
-                  }*/
       }
     }]
   );
