464273ac7355fc47e67d8cdd1713344ff16c7ab1
mcverter <mitchell.verter@gmail.com>
2014-12-29 09:10:31 -0500

    fixing wizard

--- public/modules/resources/thing/taxonomy/common/rsc_resourceDetailsController.js
+++
@@ -1,9 +0,0 @@
-(function (angular, app) {
-    'use strict';
-
-    app.controller('ResourceDetailsController', ['$scope', 'ResourceTaxonomyService',
-        function ($scope, ResourceTaxonomyService) {
-            console.log(ResourceTaxonomyService);
-
-    }]);
-})(window.angular, window.guttersnipe);
--- public/modules/resources/thing/taxonomy/common/rsc_taxonomyGeneralDirective.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by mitchell on 12/18/2014.
- */
