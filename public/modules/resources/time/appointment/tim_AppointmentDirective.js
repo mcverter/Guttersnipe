a9db2ca50213fa05c6b2a14c4114a019c89ee664
mcverter <mitchell.verter@gmail.com>
2015-01-11 10:05:17 -0500

    returning to Punctual/Weekly/Yearly as sub components.   Adding Appointment to cover them

---
+++ public/modules/resources/time/appointment/tim_AppointmentDirective.js
@@ -0,0 +1,19 @@
+
+(function (angular, app) {
+  'use strict';
+
+  app.directive('appointment', ['filePaths', function(filePaths) {
+      var templateUrl = filePaths.resources_dir + "time/appointment/tim_AppointmentWidget.html";
+
+      return {
+        restrict: 'E',
+        templateUrl: templateUrl,
+        scope : {
+          scheduleType: '='
+        }
+      }
+    }]
+  );
+})(window.angular, window.guttersnipe);
+
+
---
+++ public/modules/resources/time/appointment/tim_AppointmentListDirective.js
@@ -0,0 +1,3 @@
+/**
+ * Created by mitchell on 1/11/2015.
+ */
