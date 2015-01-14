25acbc38b348c73c4c18cd05382888b1464227b9
mcverter <mitchell.verter@gmail.com>
2015-01-13 20:14:11 -0500

    developing rename script

--- recursiveRename.sh
+++ recursiveRename.sh
@@ -1,3 +1,16 @@
+#!/bin/bash
+
 echo "foo"
 
-find . -type f -exec rename 's/Widget/Template/' '{}' \;
+
+echo "My name is $1 and I come from $2"
+
+if [ $# -lt 2 ]
+    then
+        echo "$0 error: you must supply two arguments"
+    else
+        echo "My name is $1 and I come from $2"
+fi
+
+
+# find . -type f -exec rename 's/Widget/Template/' '{}' \;
