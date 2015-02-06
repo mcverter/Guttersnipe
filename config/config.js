cf0e8ef0329a559199f8cac74b7748d7e36f4c5c
mcverter@github.com <mitchell.verter@gmail.com>
2015-02-06 01:36:51 -0500

    Javascript fileglob fix

--- config/config.js
+++ config/config.js
@@ -58,7 +58,7 @@ module.exports.getGlobbedFiles = function(globPatterns, removeRoot) {
  */
 module.exports.getJavaScriptAssets = function(includeTests) {
 	var output = this.getGlobbedFiles(this.assets.lib.js.concat(this.assets.js), 'public/');
-
+console.log('output javascript assets are ' + output);
 	// To include tests
 	if (includeTests) {
 		output = _.union(output, this.getGlobbedFiles(this.assets.tests));
