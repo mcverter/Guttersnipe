d1b9fb8122c9136cdefd06bb0d3d80c8b49c1b7a
mcverter@github.com <mitchell.verter@gmail.com>
2015-02-06 00:37:26 -0500

    fixing heroku by setting NODE_ENV to production

--- config/init.js
+++ config/init.js
@@ -24,7 +24,7 @@ module.exports = function() {
 				console.error(chalk.red('NODE_ENV is not defined! Using default development environment'));
 			}
 
-			process.env.NODE_ENV = 'development';
+			process.env.NODE_ENV = 'production';
 		} else {
 			console.log(chalk.black.bgWhite('Application loaded using the "' + process.env.NODE_ENV + '" environment configuration'));
 		}
