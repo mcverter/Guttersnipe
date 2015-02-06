545c914860ab6991d59a947031f7e3b5e4255ebc
mcverter@github.com <mitchell.verter@gmail.com>
2015-02-06 00:58:01 -0500

    changing mongodb in production

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/guttersnipe',
+	db: 'mongodb://mongo_guttersnipe:guttersnipe_mongo@ds061370.mongolab.com:61370/guttersnipe',
 	assets: {
 		lib: {
 			css: [
