1ed3d3847d477ff902ebb969883b7389c583316c
mcverter <mitchel.verter@gmail.com>
2015-02-09 11:21:04 -0500

    Overwriting config/production.js in hopes of fixing heroku fileglob

--- config/env/production.js
+++ config/env/production.js
@@ -2,27 +2,38 @@
 
 module.exports = {
 	db: 'mongodb://mongo_guttersnipe:guttersnipe_mongo@ds061370.mongolab.com:61370/guttersnipe',
-	assets: {
-		lib: {
-			css: [
-				'public/lib/bootstrap/dist/css/bootstrap.min.css',
-				'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
-			],
-			js: [
-				'public/lib/angular/angular.min.js',
-				'public/lib/angular-resource/angular-resource.js', 
-				'public/lib/angular-cookies/angular-cookies.js', 
-				'public/lib/angular-animate/angular-animate.js', 
-				'public/lib/angular-touch/angular-touch.js', 
-				'public/lib/angular-sanitize/angular-sanitize.js', 
-				'public/lib/angular-ui-router/release/angular-ui-router.min.js',
-				'public/lib/angular-ui-utils/ui-utils.min.js',
-				'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js'
-			]
-		},
-		css: 'public/dist/application.min.css',
-		js: 'public/dist/application.min.js'
-	},
+    assets: {
+        lib: {
+            css: [
+                'public/lib/bootstrap/dist/css/bootstrap.css',
+                'public/lib/bootstrap/dist/css/bootstrap-theme.css',
+            ],
+            js: [
+                'public/lib/angular/angular.js',
+                'public/lib/angular-resource/angular-resource.js',
+                'public/lib/angular-cookies/angular-cookies.js',
+                'public/lib/angular-animate/angular-animate.js',
+                'public/lib/angular-touch/angular-touch.js',
+                'public/lib/angular-sanitize/angular-sanitize.js',
+                'public/lib/angular-ui-router/release/angular-ui-router.js',
+                'public/lib/angular-ui-utils/ui-utils.js',
+                'public/lib/angular-bootstrap/ui-bootstrap-tpls.js'
+            ]
+        },
+        css: [
+            'public/modules/**/css/*.css'
+        ],
+        js: [
+            'public/config.js',
+            'public/application.js',
+            'public/modules/*/*.js',
+            'public/modules/*/*[!tests]*/*.js'
+        ],
+        tests: [
+            'public/lib/angular-mocks/angular-mocks.js',
+            'public/modules/*/tests/*.js'
+        ]
+    },
 	facebook: {
 		clientID: process.env.FACEBOOK_ID || 'APP_ID',
 		clientSecret: process.env.FACEBOOK_SECRET || 'APP_SECRET',
