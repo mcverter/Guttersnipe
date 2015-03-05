b4b06656168d84ce3302f8e45e19d007d106138f
mcverter@github.com <mitchell.verter@gmail.com>
2015-03-05 16:12:56 -0500

    adding full client side representation for resources

---
+++ public/modules/resources/directives/gut.resource.method.client.directive.js
@@ -0,0 +1,14 @@
+(function (angular, _) {
+  'use strict';
+
+  angular.module('resources').directive('gutResourceMethod', ['resource_templates', function(templates) {
+      var templateUrl = templates.main + 'resource-method.template.html';
+
+      return {
+        restrict: 'E',
+        templateUrl: templateUrl
+      }
+    }]
+  );
+})(window.angular, window._);
+
