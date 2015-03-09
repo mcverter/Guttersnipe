0303c1870f00b3a9928612bfa71f1fb8fc6d58cb
mcverter@github.com <mitchell.verter@gmail.com>
2015-03-09 09:37:40 -0400

    fixing images in presentation

---
+++ public/modules/docs/config/doc.client.config.js
@@ -0,0 +1,5 @@
+
+var PRESENTATION_IMAGE_DIR = 'modules/docs/templates/CCNYProposal/images';
+
+angular.module('docs')
+  .constant('presentation2013images', PRESENTATION_IMAGE_DIR);
--- public/modules/docs/config/docs.client.routes.js
+++ public/modules/docs/config/docs.client.routes.js
@@ -43,57 +43,57 @@
          url: '/docs/presentation2013',
          templateUrl: PRESENTATION_DIR + 'presentation.html'
          })            */
-        .state('CCNY.front', {
-          url: '/front',
+        .state('CCNY_front', {
+          url: '/docs/presentation2013/front',
           templateUrl: PRESENTATION_DIR + 'front.html'
         })
-        .state('CCNY.objective', {
-          url: '/objective',
+        .state('CCNY_objective', {
+          url: '/docs/presentation2013/objective',
           templateUrl: PRESENTATION_DIR + 'objective.html'
         })
 
-        .state('CCNY.audience', {
-          url: '/audience',
+        .state('CCNY_audience', {
+          url: '/docs/presentation2013/audience',
           templateUrl: PRESENTATION_DIR + 'audience.html'
         })
 
-        .state('CCNY.consultations', {
-          url: '/consultations',
+        .state('CCNY_consultations', {
+          url: '/docs/presentation2013/consultations',
           templateUrl: PRESENTATION_DIR + 'consultations.html'
         })
 
-        .state('CCNY.otherSites', {
-          url: '/otherSites',
+        .state('CCNY_otherSites', {
+          url: '/docs/presentation2013/otherSites',
           templateUrl: PRESENTATION_DIR + 'otherSites.html'
         })
 
-        .state('CCNY.questionOfMethod', {
-          url: '/questionOfMethod',
+        .state('CCNY_questionOfMethod', {
+          url: '/docs/presentation2013/questionOfMethod',
           templateUrl: PRESENTATION_DIR + 'questionOfMethod.html'
         })
 
-        .state('CCNY.answerOfMethod', {
-          url: '/answerOfMethod',
+        .state('CCNY_answerOfMethod', {
+          url: '/docs/presentation2013/answerOfMethod',
           templateUrl: PRESENTATION_DIR + 'answerOfMethod.html'
         })
-        .state('CCNY.example', {
-          url: '/example',
+        .state('CCNY_example', {
+          url: '/docs/presentation2013/example',
           templateUrl: PRESENTATION_DIR + 'example.html'
         })
-        .state('CCNY.report', {
-          url: '/report',
+        .state('CCNY_report', {
+          url: '/docs/presentation2013/report',
           templateUrl: PRESENTATION_DIR + 'report.html'
         })
-        .state('CCNY.result', {
-          url: '/result',
+        .state('CCNY_result', {
+          url: '/docs/presentation2013/result',
           templateUrl: PRESENTATION_DIR + 'result.html'
         })
-        .state('CCNY.research', {
-          url: '/research',
+        .state('CCNY_research', {
+          url: '/docs/presentation2013/research',
           templateUrl: PRESENTATION_DIR + 'research.html'
         })
-        .state('CCNY.fin', {
-          url: '/fin',
+        .state('CCNY_fin', {
+          url: '/docs/presentation2013/fin',
           templateUrl: PRESENTATION_DIR + 'fin.html'
         });
     }
