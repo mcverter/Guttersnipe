105ab58b49e6c5393f191b178a4c226c29637471
mcverter@github.com <mitchell.verter@gmail.com>
2015-03-10 17:55:17 -0400

    moving back to old form of  declaration

--- public/modules/resources/controllers/resources.client.controller.js
+++ public/modules/resources/controllers/resources.client.controller.js
@@ -9,98 +9,108 @@
       Object.defineProperties($scope, {
         resource: {
           enumerable: true,
-          get: function() {return resource;},
-          set: function(val){resource = val}
-        },
-        authentication : {
-          enumerable: true,
-          get: function() {return Authentication;},
-          set: function(val){Authentication = val}
-
-        },
-        create : function() {
-          var resource = new Resources({
-            resource: this.resource
-          });
-          resource.$save(function(response) {
-            $location.path('resources/' + response._id);
-
-            $scope.resource = {};
-          }, function(errorResponse) {
-            $scope.error = errorResponse.data.message;
-          });
-        },
-  remove : function(resource) {
-    if (resource) {
-      resource.$remove();
-
-      for (var i in $scope.resources) {
-        if ($scope.resources[i] === resource) {
-          $scope.resources.splice(i, 1);
-        }
-      }
-    } else {
-      $scope.resource.$remove(function() {
-        $location.path('resources');
-      });
-    }
-  },
-        update : function() {
-          var resource = $scope.resource;
-
-          resource.$update(function() {
-            $location.path('resources/' + resource._id);
-          }, function(errorResponse) {
-            $scope.error = errorResponse.data.message;
-          });
-        },
-        find : function() {
-          $scope.resources = Resources.query();
-        },
-  findOne : function() {
-    Resources.get({
-      resourceId: $stateParams.resourceId
-    }).$promise.then(function(rsc) {
-        var createAgreed = false;
-
-
-        $scope.resource = '';
-
-        $scope.resource = rsc;
-        $scope.options = {scrollwheel: false};
-        var coordinates = $scope.resource.place.coordinates;
-
-        Object.defineProperties($scope, {
-            createAgreed: {
-              enumerable: true,
-              get: function getCreateAgreed() {
-                return createAgreed;
-              },
-              set: function setCreateAgreed(val) {
-                return createAgreed = val;
-              }
-            },
-          map : {
-            center: {
-              latitude: coordinates.lat,
-              longitude: coordinates.lng
-            },
-            marker : {
-              id: 0,
-              coords: {
-                latitude: coordinates.lat,
-                longitude: coordinates.lng
-              },
-              options: { draggable: false }
-            },
-            zoom: 16
+          get: function () {
+            return resource;
+          },
+          set: function (val) {
+            resource = val
           }
+        },
+        authentication: {
+          enumerable: true,
+          get: function () {
+            return Authentication;
+          },
+          set: function (val) {
+            Authentication = val
+          }
+
+        }
+      });
+      $scope.create =  function()
+      {
+        var resource = new Resources({
+          resource: this.resource
         });
-      });
-  }
+        resource.$save(function (response) {
+          $location.path('resources/' + response._id);
 
+          $scope.resource = {};
+        }, function (errorResponse) {
+          $scope.error = errorResponse.data.message;
+        });
+      },
+        $scope.remove = function(resource) {
+          if (resource) {
+            resource.$remove();
 
-      });
+            for (var i in $scope.resources) {
+              if ($scope.resources[i] === resource) {
+                $scope.resources.splice(i, 1);
+              }
+            }
+          } else {
+            $scope.resource.$remove(function() {
+              $location.path('resources');
+            });
+          }
+        };
+      $scope.update = function() {
+        var resource = $scope.resource;
+
+        resource.$update(function() {
+          $location.path('resources/' + resource._id);
+        }, function(errorResponse) {
+          $scope.error = errorResponse.data.message;
+        });
+      };
+
+      $scope.find = function() {
+        $scope.resources = Resources.query();
+      };
+
+      $scope.findOne = function() {
+        Resources.get({
+          resourceId: $stateParams.resourceId
+        }).$promise.then(function(rsc) {
+            var createAgreed = false;
+
+            console.log("receourse gotten is ", rsc)
+
+            $scope.resource = rsc;
+            $scope.options = {scrollwheel: false};
+            var coordinates = $scope.resource.place.coordinates;
+            console.log("coords is ", coordinates)
+
+            Object.defineProperties($scope, {
+              createAgreed: {
+                enumerable: true,
+                get: function getCreateAgreed() {
+                  return createAgreed;
+                },
+                set: function setCreateAgreed(val) {
+                  return createAgreed = val;
+                }
+              }
+            });
+              $scope.map = {
+                center: {
+                  latitude: coordinates.lat,
+                  longitude: coordinates.lng
+                },
+                marker : {
+                  id: 0,
+                  coords: {
+                    latitude: coordinates.lat,
+                    longitude: coordinates.lng
+                  },
+                  options: { draggable: false }
+                },
+                zoom: 16
+              }
+            console.log("scoop", $scope);
+          });
+      };
     }
   ]);
 })(window.angular, window._);
