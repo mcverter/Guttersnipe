43dd8a2e74b14710e88faba63eafc8f625616dcb
mcverter@github.com <mitchell.verter@gmail.com>
2015-04-07 18:07:48 -0400

    fixed time code

--- public/modules/times/directives/time.create-schedule.client.directive.js
+++ public/modules/times/directives/time.create-schedule.client.directive.js
@@ -1,92 +1,86 @@
 
 (function () {
-    'use strict';
+  'use strict';
 
 
-    angular.module('times')
-        .directive('createSchedule',['times_templates', function(templates) {
-            var templateUrl = templates.main + 'time.schedule-input.client.template.html',
-                MSEC_TO_30MIN = 60 * 30 * 1000,
-                eventSources = [];
+  angular.module('times')
+    .directive('createSchedule',['times_templates', function(templates) {
+      var templateUrl = templates.main + 'time.schedule-input.client.template.html',
+        MSEC_TO_30MIN = 60 * 30 * 1000,
+        eventSources = [], gutEvent;
 
 
 
-            function ScheduleModalController ($scope, $modalInstance) {
-                $scope.start;
-                $scope.repeating = false;
-                $scope.addSchedule = addSchedule;
-                $scope.ok = okSchedule;
-                $scope.cancelSchedule = cancelSchedule
+      function ScheduleModalController ($scope, $modalInstance) {
+        $scope.addSchedule = addSchedule;
+        $scope.ok = okSchedule;
+        $scope.cancelSchedule = cancelSchedule;
+        $scope.gutEvent = gutEvent;
 
-                function okSchedule() {
-                    $modalInstance.close($scope.selected.item);
-                }
+        function okSchedule() {
+          $modalInstance.close($scope.selected.item);
+        }
 
-                function addSchedule(e, start, end, repeating) {
-                    e.preventDefault();
-                    eventSources.push({
-                        title: 'event2',
-                        start: start,
-                        end: end,
-                        repeating: repeating
-                    });
-                    $modalInstance.dismiss('added');
-                }
+        function addSchedule(e, gutEvent) {
+          e.preventDefault();
+          eventSources.push(gutEvent);
+          $modalInstance.dismiss('added');
+        }
 
-                function cancelSchedule (event) {
-                    event.preventDefault();
-                    $modalInstance.dismiss('cancel');
-                }
+        function cancelSchedule (event) {
+          event.preventDefault();
+          $modalInstance.dismiss('cancel');
+        }
+      }
+
+      function CreateScheduleController($scope, $modal) {
+        $scope.time.schedules = eventSources;
+        $scope.punctualConfig = {
+          calendar: {
+            height: 450,
+            editable: true,
+            header: {
+              left: 'month agendaWeek agendaDay',
+              center: 'title',
+              right: 'today prev,next'
+            },
+            defaultView: 'agendaWeek',
+            dayClick: function (clickedTime) {
+              var modalInstance;
+
+              clickedTime.setHours(12);
+              gutEvent = {
+                start: clickedTime,
+                end: new Date(clickedTime.getTime() + MSEC_TO_30MIN),
+                repeating: false
+              }
+
+              console.log('gut Event is', gutEvent);
+
+              modalInstance = $modal.open({
+                templateUrl: templates.main +
+                  'time.schedule-dialog.client.template.html',
+                controller: ScheduleModalController
+              });
+
+              modalInstance.result.then(function (selectedItem) {
+                $scope.selected = selectedItem;
+              }, function () {
+                console.log('Modal dismissed at: ' + new Date());
+              });
             }
-
-            function CreateScheduleController($scope, $modal) {
-                $scope.eventSources = eventSources;
-                $scope.punctualConfig = {
-                    calendar: {
-                        height: 450,
-                        editable: true,
-                        header: {
-                            left: 'month agendaWeek agendaDay',
-                            center: 'title',
-                            right: 'today prev,next'
-                        },
-                        defaultView: 'agendaWeek',
-                        dayClick: function (start) {
-                            start.setHours(12);
-                            console.log(start);
-                            var end = angular.copy(start);
-                            var repeating = false;
-                            end.setTime(start.getTime() + MSEC_TO_30MIN);
-
-                            var modalInstance = $modal.open({
-                                templateUrl: templates.main + 'time.schedule-dialog.client.template.html',
-                                controller: ScheduleModalController
-                            });
-
-                            modalInstance.result.then(function (selectedItem) {
-                                $scope.selected = selectedItem;
-                            }, function () {
-                                console.log('Modal dismissed at: ' + new Date());
-                            });
-                        }
-                    }
-                }
-            }
-
-
-
-
-
-
-            return {
-                restrict: 'E',
-                templateUrl: templateUrl,
-                controller: CreateScheduleController,
-                scope : {
-                    confirmations: '=',
-                    time: '='
-                }
-            };
-        }]
-    );
+          }
+        }
+      }
+      return {
+        restrict: 'E',
+        templateUrl: templateUrl,
+        controller: CreateScheduleController,
+        scope : {
+          confirmations: '=',
+          time: '='
+        }
+      };
+    }]
+  );
 })();
