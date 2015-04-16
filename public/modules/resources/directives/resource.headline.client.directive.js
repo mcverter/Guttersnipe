68a974a237a43eb4b18c0683451612dac76e0b10
mcverter <mitchell.verter@gmail.com>
2015-04-16 15:16:58 -0400

    fixing headline

--- public/modules/resources/directives/resource.headline.client.directive.js
+++ public/modules/resources/directives/resource.headline.client.directive.js
@@ -1,14 +1,17 @@
 (function () {
-  'use strict';
+    'use strict';
 
-  angular.module('resources').directive('resourceHeadline', ['resource_templates', function(templates) {
-      var templateUrl = templates.main + 'read/' +'resource-headline.template.html';
+    angular.module('resources').directive('resourceHeadline', ['resource_templates', function(templates) {
+            var templateUrl = templates.main + 'read/' +'resource-headline.template.html';
 
-      return {
-        restrict: 'E',
-        templateUrl: templateUrl
-      };
-    }]
-  );
+            return {
+                restrict: 'E',
+                templateUrl: templateUrl,
+                scope : {
+                    headline: '='
+                }
+            };
+        }]
+    );
 })();
 
