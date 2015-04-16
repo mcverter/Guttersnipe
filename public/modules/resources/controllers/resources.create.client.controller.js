9c35ef281bf1929cf0fb49c368172395ed24eb36
mcverter@github.com <mitchell.verter@gmail.com>
2015-04-16 09:00:00 -0400

    application redirects to resources all page after a create

--- public/modules/resources/controllers/resources.create.client.controller.js
+++ public/modules/resources/controllers/resources.create.client.controller.js
@@ -1,70 +1,60 @@
 
 (function() {
-    'use strict';
+  'use strict';
 
-    function ResourceCreateController($scope, Resources, $location, $anchorScroll, Authentication) {
-        $scope.activate = activate;
-        $scope.create = createResource;
+  function ResourceCreateController($scope, Resources, $state, $location, $anchorScroll, Authentication) {
+    $scope.activate = activate;
+    $scope.create = createResource;
+    $scope.resource = {};
+    $scope.scrollTo = scrollTo;
+    $scope.confirmations = {
+      isCreatePlaceConfirmed : false,
+      isCreateTimeConfirmed : false,
+      isCreateDescriptionConfirmed : false,
+      isCreateClassificationConfirmed : false
+    };
 
-        $scope.resource = {};
+    activate();
 
-        $scope.scrollTo = scrollTo;
-
-        $scope.confirmations = {
-            isCreatePlaceConfirmed : false,
-            isCreateTimeConfirmed : false,
-            isCreateDescriptionConfirmed : false,
-            isCreateClassificationConfirmed : false
-        };
-
-        activate();
-
-        function activate() {
-            $scope.resource = Resources.getEmptyResource();
-            console.log('resource is', $scope.resource);
-        }
-
-        function isCreateFormConfirmed() {
-            return  $scope.confirmations.isCreatePlaceConfirmed &&
-                $scope.confirmations.isCreateTimeConfirmed &&
-                $scope.confirmations.isCreateDescriptionConfirmed &&
-                $scope.confirmations.isCreateClassificationConfirmed;
-        }
-
-        function scrollTo(id) {
-            $location.hash(id);
-            $anchorScroll();
-        }
-
-        function createResource() {
-
-            if (isCreateFormConfirmed()) {
-                Resources.createResource($scope.resource)
-                    .then(function (data) {
-                        console.log('Returned data', data)
-                    })
-                    .catch(function (err) {
-                        console.err('got error', err);
-                    });
-            }
-            else {
-                $scope.showPlaceConfirmError =  ! $scope.confirmations.isCreatePlaceConfirmed;
-                $scope.showTimeConfirmError =   ! $scope.confirmations.isCreateTimeConfirmed;
-                $scope.showDescriptionConfirmError =  ! $scope.confirmations.isCreateDescriptionConfirmed;
-                $scope.showClassificationConfirmError =  ! $scope.confirmations.isCreateClassificationConfirmed;
-            }
-        }
-
+    function activate() {
+      $scope.resource = Resources.getEmptyResource();
     }
 
+    function isCreateFormConfirmed() {
+      return  $scope.confirmations.isCreatePlaceConfirmed &&
+        $scope.confirmations.isCreateTimeConfirmed &&
+        $scope.confirmations.isCreateDescriptionConfirmed &&
+        $scope.confirmations.isCreateClassificationConfirmed;
+    }
 
+    function scrollTo(id) {
+      $location.hash(id);
+      $anchorScroll();
+    }
 
-    /*
-     Resource is {"thing":{"description":{"summary":"It's a very miserable place","notes":"Cry all the time","headline":"Mitchell's House","method":"Go there and beg"},"taxonomy":{"type":"food","subtypes":["Food Not Bombs","Free Communal Meal"]},"details":["moose"]},"place":{"coordinates":{"lat":40.758895,"lng":-73.98513100000002},"name":"","address":"Times Square, Manhattan, NY 10036, USA","notes":"moo","description":"describer"},"time":{"schedules":[{"repeating":false,"id":1,"allDay":false,"start":"2015-04-07T12:00:00.000Z","end":"2015-04-07T12:30:00.000Z","__uiCalId":1,"_id":"1","_start":"2015-04-07T12:00:00.000Z","_end":"2015-04-07T12:30:00.000Z","className":[]}],"notes":""}}
-     */
+    function createResource() {
+      if (isCreateFormConfirmed()) {
+        Resources.createResource($scope.resource)
+          .then(function (data) {
+            console.log('Returned data', data);
+            $state.go('allResources');
+          })
+          .catch(function (err) {
+            console.err('got error', err);
+          });
+      }
+      else {
+        $scope.showPlaceConfirmError =  ! $scope.confirmations.isCreatePlaceConfirmed;
+        $scope.showTimeConfirmError =   ! $scope.confirmations.isCreateTimeConfirmed;
+        $scope.showDescriptionConfirmError =  ! $scope.confirmations.isCreateDescriptionConfirmed;
+        $scope.showClassificationConfirmError =  ! $scope.confirmations.isCreateClassificationConfirmed;
+      }
+    }
 
-    angular.module('resources')
-        .controller('ResourcesCreateController',
-        ['$scope', 'Resources', '$location', '$anchorScroll', 'Authentication', ResourceCreateController])
+  }
+
+  angular.module('resources')
+    .controller('ResourcesCreateController',
+    ['$scope', 'Resources', '$state', '$location', '$anchorScroll', 'Authentication', ResourceCreateController])
 
 })();
--- server/models/resource.server.model.js
+++ server/models/resource.server.model.js
@@ -4,109 +4,127 @@
  * Module dependencies.
  */
 var mongoose = require('mongoose'),
-    Schema = mongoose.Schema;
+  Schema = mongoose.Schema;
 
 // node add-freegan-bk.js 
 var recurrenceTypeEnum = ['A', '1', '2', '3', '4', 'L'];
 
 
 var ResourceSchema = new Schema({
-    thing: {
-        description: {
-            headline : {
-                type: String,
-                required: true
-            },
-            summary : {
-                type: String,
-                required: true
-            },
-            method: {
-                type: String,
-                required: false
-            },
-            notes : {
-                type: String,
-                required: false
-            }
-        },
-        taxonomy : {
-            type : {
-                type: String,
-                required: true
-            },
-            subtypes : [String],
-            details : [String]
-        }
-    },
-    place: {
-        address : {
-            type: String,
-            required: true
-        },
-        coordinates : {
-            lat : {
-                type: Number,
-                required: true
-            },
-            lng : {
-                type: Number,
-                required: true
-            }
-        },
-        description : {
-            type: String,
-            required: false
-        },
-        notes : {
-            type: String,
-            required: false
-        }
-    },
-
-
-    time: {
-        schedules : [
-            {
-                recurrenceType: {
-                    type: Boolean,
-                    enum: recurrenceTypeEnum
-                },
-                start : {
-                    type: Date,
-                    required: true
-                },
-                end : {
-                    type: Date,
-                    required: true
-                }
-            }
-        ],
-        notes : {
-            type: String,
-            required: false
-        }
-    },
-
-
-    created: {
-        type: Date,
-        default: Date.now
-    },
-
-    updated: {
-        type: Date,
-        default: Date.now
-    },
-
-    createdBy: {
+  rating: {
+    type: Number
+  },
+  comments :
+    [{
+      comment: {
+        type: String,
+        required: true
+      },
+      author: {
         type: Schema.ObjectId,
         ref: 'User'
+      },
+      posted: {
+        type: Date,
+        default: Date.now
+      }
+    }],
+  thing: {
+    description: {
+      headline : {
+        type: String,
+        required: true
+      },
+      summary : {
+        type: String,
+        required: true
+      },
+      method: {
+        type: String,
+        required: false
+      },
+      notes : {
+        type: String,
+        required: false
+      }
     },
-    updatedBy: {
-        type: Schema.ObjectId,
-        ref: 'User'
+    taxonomy : {
+      type : {
+        type: String,
+        required: true
+      },
+      subtypes : [String],
+      details : [String]
     }
+  },
+  place: {
+    address : {
+      type: String,
+      required: true
+    },
+    coordinates : {
+      lat : {
+        type: Number,
+        required: true
+      },
+      lng : {
+        type: Number,
+        required: true
+      }
+    },
+    description : {
+      type: String,
+      required: false
+    },
+    notes : {
+      type: String,
+      required: false
+    }
+  },
+
+
+  time: {
+    schedules : [
+      {
+        recurrenceType: {
+          type: Boolean,
+          enum: recurrenceTypeEnum
+        },
+        start : {
+          type: Date,
+          required: true
+        },
+        end : {
+          type: Date,
+          required: true
+        }
+      }
+    ],
+    notes : {
+      type: String,
+      required: false
+    }
+  },
+
+
+  created: {
+    type: Date,
+    default: Date.now
+  },
+
+  updated: {
+    type: Date,
+    default: Date.now
+  },
+
+  createdBy: {
+    type: Schema.ObjectId,
+    ref: 'User'
+  },
+  updatedBy: {
+    type: Schema.ObjectId,
+    ref: 'User'
+  }
 });
 
 mongoose.model('Resource', ResourceSchema);
