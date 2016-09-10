15246b90a4b319cd7a6c3ffd66e994ad5977e838
mcverter@github.com <mitchell.verter@gmail.com>
2016-09-10 09:27:21 -0400

    fixing scope resolutoin for kropotkin directive

--- public/modules/kropotkins/directives/kropotkin-quote.client.directive.js
+++ public/modules/kropotkins/directives/kropotkin-quote.client.directive.js
@@ -1,32 +1,40 @@
+/**
+ * Created by mitchell_verter on 9/9/16.
+ */
 (function () {
     'use strict';
 
-    // controller
+    angular
+      .module('kropotkins')
+      .directive('kropotkinQuote', kropotkinDirective)
+      .controller('KropotkinAnarchyIsOrder', KropotkinAnarchyIsOrder);
+
+    function kropotkinDirective () {
+        return {
+            restrict: 'E',
+            templateUrl:  'modules/kropotkins/templates/kropotkinDirective.view.html',
+            controller: 'KropotkinAnarchyIsOrder'
+        };
+    }
+
+    KropotkinAnarchyIsOrder.$inject = ['$scope', 'Kropotkins'];
+
     function KropotkinAnarchyIsOrder ($scope, Kropotkins) {
         var loadQuote = function loadQuote() {
             Kropotkins.getRandom()
-                .then(function(data) {
-                    $scope.quote = data;
-                    console.log($scope.quote);
-                }, function(err) {
-                    console.log('Could not load quote', err);
-                });
+              .then(function(data) {
+                  $scope.quote = data;
+                  console.log($scope.quote);
+              }, function(err) {
+                  console.log('Could not load quote', err);
+              });
         };
 
         loadQuote();
 
         $scope.reloadQuote =  loadQuote;
     }
-
-
-    function kropotkinDirective () {
-        return {
-            restrict: 'E',
-            templateUrl:  'modules/kropotkins/templates/kropotkinDirective.view.html',
-            controller: ['$scope', 'Kropotkins',
-                KropotkinAnarchyIsOrder ($scope, Kropotkins)]
-        };
-    }
-
-    angular.module('kropotkins').directive('kropotkinQuote', ['Kropotkins', kropotkinDirective]);
 })();
+
+/* OR */
+//var foo = {controller: ['$scope', 'Kropotkins', KropotkinAnarchyIsOrder]}
