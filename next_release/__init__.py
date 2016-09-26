26d4b911d7c635ec41839879c23f711579c6b03a
mitchell <mitchell@roadrunner.waste>
2016-09-25 21:13:20 -0400

    adding flask sql alchemy in tutorial

--- next_release/__init__.py
+++ next_release/__init__.py
@@ -1,10 +1,11 @@
-__author__ = 'mitchell_verter'
-
 from flask import Flask
-from flask.ext.sqlalchemy import SQLAlchemy
+
+from flask.ext.sqlachemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('config')
+
 db = SQLAlchemy(app)
 
 from app import views, models
+
---
+++ next_release/db_downgrade.py
@@ -0,0 +1,6 @@
+from migrate.versioning import api
+from config import SQLALCHEMY_DATABASE_URI
+from config import SQLALCHEMY_MIGRATE_REPO
+api.downgrade(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO, v-1)
+v = api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+print ('current db version' + str(v))
