d4feeecd46b7341acc319e31facef4fe47d56066
mcverter <mitchell.verter@gmail.com>
2016-10-31 17:25:02 -0400

    arranging db migration

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,14 @@ __author__ = 'mitchell_verter'
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__,
             static_url_path='/public')
 app.config.from_object('config')
 # app.config.from_pyfile('config.py')
 db = SQLAlchemy(app)
+
+
+import models.user_models
+import models.icalendar_models
+import models.shareable_models
