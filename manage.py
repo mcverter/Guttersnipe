30e2ca0bc3c5b293f5a39bbbc944785351f38739
mcverter@github.com <mitchell.verter@gmail.com>
2016-11-15 23:46:56 -0500

    developing seeds

--- manage.py
+++ manage.py
@@ -3,6 +3,7 @@ __author__ = 'mitchell_verter'
 from flask_script import Manager, Shell
 from flask.ext.migrate import Migrate, MigrateCommand
 import os
+import importlib
 
 from app import app, db
 
@@ -22,10 +23,10 @@ def seed():
     dir_path = dir_path = os.path.dirname(os.path.realpath(__file__))
     seed_path = os.path.join(dir_path, "db", "seeds")
 
-    for seed_file in os.listdir(seed_path):
-        pass
-
-
+    for file in os.listdir(seed_path):
+        print ("file", file)
+        if file[-3:] == ".py":
+            importlib.import_module('db.seeds.' + file[0:-3])
 
 if __name__ == '__main__':
     manager.run()
