319b5d2c1486bbaf6308bb3f5b8a570913c15054
mcverter <mitchell.verter@gmail.com>
2016-11-16 10:02:46 -0500

    making sure manage.py survived merge

--- manage.py
+++ manage.py
@@ -3,6 +3,7 @@ __author__ = 'mitchell_verter'
 from flask_script import Manager, Shell
 from flask.ext.migrate import Migrate, MigrateCommand
 import os
+import importlib
 
 from app import app, db
 
@@ -16,14 +17,31 @@ migrate = Migrate(app, db, directory="db/migrations")
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
+'''
+Makeshift solution for seeding database.
+Grinberg recommends doing seeding through the flask-migrate file
+http://stackoverflow.com/questions/19334604/creating-seed-data-in-a-flask-migrate-or-alembic-migration
 
+This solution places all seed files in a dedicated directory and imports/executes each module.
+
+The modules will have the form
+===============================
+...
+...
+def seed():
+    blah blah
+
+if __name__ == '__main__':
+    seed()
+'''
 @manager.command
 def seed():
     dir_path = dir_path = os.path.dirname(os.path.realpath(__file__))
     seed_path = os.path.join(dir_path, "db", "seeds")
 
     for seed_file in os.listdir(seed_path):
-        pass
+        if seed_file[-3:] == ".py":
+            importlib.import_module("app.db.seeds" + seed_file[0:-3])
 
 
 
