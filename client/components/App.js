c501b4885f57e356becbfa5ee20b03ec63766bd3
mcverter@github.com <mitchell.verter@gmail.com>
2017-01-04 11:31:01 -0500

    adding more front end structure

--- client/components/App.js
+++ client/components/App.js
@@ -1,10 +1,13 @@
 import React, {PropTypes} from 'react';
-
+import Footer from './common/Footer';
+import Header from './common/Header'
 class App extends React.Component {
   render() {
     return (
       <div className="container-fluid">
+          <Header />
         {this.props.children}
+          <Footer />
       </div>
     );
   }
--- client/components/common/Footer.js
+++ client/components/common/Footer.js
@@ -1,3 +1,34 @@
-/**
- * Created by mitchell on 1/4/2017.
+import React, {PropTypes} from 'react';
+
+export default Footer = (user) => {
+    return (
+        <div> Footer is footer </div>
+    )
+}
+Footer.propTypes = {
+    user: PropTypes.object
+}
+
+/*
+<div class="navbar navbar-default navbar-fixed-bottom" role="navigation">
+    <div class="container NavColor">
+        <div class="navbar-header">
+        </div>
+            <ul class="nav navbar-nav navbar-right sm">
+                <li><a ui-sref="faq" class="navbar-brand ">
+                    FAQ</a></li>
+                <li  ng-class="{active: $uiRoute}">
+                    <a ui-sref="documentation"  class="navbar-brand ">
+                        Docs</a></li>
+                <li  ng-class="{active: $uiRoute}">
+                    <a ui-sref="illlegal"  class="navbar-brand ">
+                        L@W</a></li>
+                <li  ng-class="{active: $uiRoute}">
+                    <a ui-sref="contact"  class="navbar-brand ">
+                        <span class="glyphicon glyphicon-envelope" aria-hidden="true"></span>
+                    </a></li>
+            </ul>
+        </div>
+    </div>
+
  */
