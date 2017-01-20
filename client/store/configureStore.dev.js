990a89ad6ad362e6470dc0402780ab333a9acce6
mcverter@github.com <mitchell.verter@gmail.com>
2017-01-20 12:37:08 -0500

    adding redux devtools to dev

--- client/store/configureStore.dev.js
+++ client/store/configureStore.dev.js
@@ -1,13 +1,15 @@
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import rootReducer from '../reducers';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 import thunk from 'redux-thunk';
 import initialState from '../reducers/initialState';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 export default function configureStore(initialState) {
   return createStore(
     rootReducer,
     initialState,
-    applyMiddleware(thunk, reduxImmutableStateInvariant())
+    composeEnhancers( applyMiddleware(thunk, reduxImmutableStateInvariant()))
   );
 }
