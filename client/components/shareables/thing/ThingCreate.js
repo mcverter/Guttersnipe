e225ab5e2c4231a7c090166f7271e267823ba6a5
mcverter@github.com <mitchell.verter@gmail.com>
2017-01-22 11:06:01 -0500

    starting to use react-select for Thing creation

--- client/components/shareables/thing/ThingCreate.js
+++ client/components/shareables/thing/ThingCreate.js
@@ -1,24 +1,61 @@
-import React, {PropTypes} from 'react';
+import React, {PropTypes, Component} from 'react';
 import { Field, reduxForm } from 'redux-form';
 import validate from '../create-wizard/validateCreateShareableWizard';
 import renderField from '../create-wizard/renderField';
+import Select from 'react-select'
+import 'react-select/dist/react-select.css';
 
-const ThingCreate = (props) => {
-    const { handleSubmit } = props;
-    return (
-        <form onSubmit={handleSubmit}>
-            <Field name="description_what" type="text" component={renderField} label="What is the shareable resource"/>
-            <Field name="description_how" type="text" component={renderField} label="How do you acquire it?"/>
-            <Field name="type" type="text" component={renderField} label="What is the type?"/>
-            <Field name="subtypes" type="text" component={renderField} label="What are the subtypes?"/>
-            <Field name="tags" type="text" component={renderField} label="What are the tags?"/>
-            <Field name="thing_notes" type="text" component={renderField} label="Additional Notes"/>
-            <div>
-                <button type="submit" className="next">Next</button>
-            </div>
-        </form>
-    );
-};
+const types_and_subtypes = {
+    "food" : [
+        "dumpster", "food not bombs", "face"
+    ],
+    "housing" : [
+        "squat", "punk house"
+    ],
+    "health" : [
+        'needle exchage', 'his clinic'
+    ]
+}
+
+const tags = ["blud", 'guts', 'onor','gawa']
+
+class ThingCreate extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+    let TypeSelect = <Select
+        name="type-select"
+        onChange={typeSelectChange}
+        options ={types_and_subtypes.keys()}
+    />;
+    let SubTypeSelect = <Select
+        name="subtype-select"
+        onChange={subtypeSelectChange}
+        options ={types_and_subtypes[this.state.selectedType]}
+    />;
+    let TagSelect = <Select
+        name="tags-select"
+        onChange={subtypeSelectChange}
+        options ={types_and_subtypes[this.state.selectedType]}
+    />;
+
+        return (
+            <form onSubmit={this.props.handleSubmit}>
+                <Field name="description_what" type="text" component={renderField} label="What is the shareable resource"/>
+                <Field name="description_how" type="text" component={renderField} label="How do you acquire it?"/>
+                <Field component={TypeSelect} />
+                <Field component={SubTypeSelect} />
+                <Field component={TagSelect} />
+                <Field name="thing_notes" type="text" component={renderField} label="Additional Notes"/>
+                <div>
+                    <button type="submit" className="next">Next</button>
+                </div>
+            </form>
+        );
+    }
+}
 
 ThingCreate.propTypes = {
     handleSubmit: PropTypes.func
