f5fd9b056a91854cc47d1c2cfab17b4ff10e9009
mcverter <mitchell.verter@gmail.com>
2017-02-11 19:12:26 -0500

    refactoring shareable schemas

--- server/shareables/schemas.py
+++ server/shareables/schemas.py
@@ -1,63 +1,26 @@
+from server import db
+
 from marshmallow import  fields
 from server.calendars.schemas import CalendarSchema
-from marshmallow_sqlalchemy import ModelSchema, ModelConverter
-from server.shareables.models import Shareable, Thing, Space, Time, MainType, Subtype
-from geoalchemy2 import Geometry, WKBElement
-from sqlalchemy import func
-from server import db
-from flask import json
-
+from marshmallow_sqlalchemy import ModelSchema
+from server.shareables.models import Shareable, Thing, Space, Time, \
+  MainType, Subtype, Comment
+from server.serializers.GeometrySerializationField import GeometrySerializationField
 
 class BaseSchema(ModelSchema):
     class Meta:
         sqla_session = db.Session
 
-class GeoConverter(ModelConverter):
-    SQLA_TYPE_MAPPING = ModelConverter.SQLA_TYPE_MAPPING.copy()
-    SQLA_TYPE_MAPPING.update({
-        Geometry: fields.Str
-    })
-
-
-class GeometrySerializationField(fields.Field):
-    def _serialize(self, value, attr, obj):
-        if value is None:
-            return value
-        else:
-            if isinstance(value, WKBElement):
-                return json.dumps({"latitude": db.session.scalar(func.ST_X(value)),
-                                   "longitude": db.session.scalar(func.ST_Y(value))})
-            else:
-                return None
-
-    def _deserialize(self, value, attr, data):
-        """Deserialize value. Concrete :class:`Field` classes should implement this method.
-
-        :param value: The value to be deserialized.
-        :param str attr: The attribute/key in `data` to be deserialized.
-        :param dict data: The raw input data passed to the `Schema.load`.
-        :raise ValidationError: In case of formatting or validation failure.
-        :return: The deserialized value.
-
-        .. versionchanged:: 2.0.0
-            Added ``attr`` and ``data`` parameters.
-        """
-        if value is None:
-            return value
-        else:
-            if isinstance(value, Geometry):
-                return {'latitude': db.session.scalar(func.ST_X(value)), 'longitude': db.session.scalar(func.ST_Y(value))}
-            else:
-                return None
-
+class CommentSchema(BaseSchema):
+  class Meta:
+    model = Comment
 
 
 class ShareableSchema(BaseSchema):
-#  space = fields.Nested('SpaceSchema')
-#  comments = fields.Nested('CommentSchema', many=True)
   thing = fields.Nested('ThingSchema')
   time = fields.Nested('TimeSchema')
   space = fields.Nested('SpaceSchema')
+  comments = fields.Nested('CommentSchema', many=True)
 
   class Meta:
     model = Shareable
@@ -90,18 +53,3 @@ class MainTypeSchema(BaseSchema):
   name = fields.String()
   class Meta:
     model = MainType
-
-
-'''
-
-class TimeSchema(ModelSc):
-    id = fields.Integer()
-    notes = fields.String()
-    calendar = fields.Nested(CalendarSchema)
-
-class CommentSchema(Schema):
-    id = fields.Integer()
-    author = fields.String()
-    text = fields.String()
-    created = fields.DateTime()
-'''
