ffbd37d643f84a8a412e829a95e724b7ce2e47ae
mcverter <mitchell.verter@gmail.com>
2017-02-24 10:33:27 -0500

    adding e2e tests

---
+++ e2e_tests/dev_deployment.js
@@ -0,0 +1,11 @@
+/**
+ * Created by mitchell on 2/24/17.
+ */
+module.exports = {
+  'Test dev deployment' : function (browser) {
+    browser
+      .url('http://localhost:3000')
+      .assert.containsText('body', 'guttersnipe')
+      .end();
+  }
+};
