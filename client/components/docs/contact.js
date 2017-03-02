eb3e8a909906cf0f5f81b33aa38a9ff111910df1
mcverter <mitchell.verter@gmail.com>
2017-03-01 19:30:08 -0500

    adding image resources from old guttersnipe

--- client/components/docs/contact.js
+++ client/components/docs/contact.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import imgsrc from '../../img/RoadrunnerAtWaste.jpg'
 
 const Contact = (props) =>  (
   <div className="text-center jumbotron RedOnBlack">
-    <a style="color: red" href="mailto:roadrunner@waste.org">
+    <a  href="mailto:roadrunner@waste.org">
+      <img src={imgsrc} /> <br />
       roadrunner [at] waste [dot] org <br />
-      <img src="modules/core/img/brand/RoadrunnerAtWaste.jpg" />
     </a>
   </div>
 );
 
 export default Contact;
+
+/*
+      <img src="modules/core/img/brand/RoadrunnerAtWaste.jpg" />
+
+ */
