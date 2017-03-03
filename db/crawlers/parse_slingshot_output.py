0944d80ba29fe7cfba90a4e12c954a172a419ed2
mcverter <mitchell.verter@gmail.com>
2017-03-03 10:57:49 -0500

    geocoding

--- db/crawlers/parse_slingshot_output.py
+++ db/crawlers/parse_slingshot_output.py
@@ -1,24 +1,27 @@
 from bs4 import BeautifulSoup
+import geocoder
 
 f = open('./slingshot_output.html', 'r')
 html_doc = f.read()
 soup = BeautifulSoup(html_doc, 'html.parser')
 contact_list = soup.find_all('dl')
 
-def get_contact(contact_list):
-  dt = contact_list
+def geocode(address) :
+  g = geocoder.google(address)
+  latlng = g.latlng
+  return latlng
 
-  name = 'dt'
-  info = 'dd'
-  dd = contact_list
+def get_contact(listing):
+  headline = listing.find('dt').text
+  contact_info = listing.find('dd').text
+  lat_lng = geocode(contact_info)
+  print(headline, '\n', contact_info, '\n\n')
+  return 1+1
 
-def is_defunct():
-  pass
+def is_defunct(listing):
+  return False
 
 for listing in contact_list:
   if is_defunct(listing):
     continue
   get_contact(listing)
-
-print (contact_list)
-
