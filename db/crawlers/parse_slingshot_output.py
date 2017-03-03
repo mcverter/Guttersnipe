52fb90acf89f6374d2c5c49055ff74bddcb9223a
mcverter <mitchell.verter@gmail.com>
2017-03-03 12:24:16 -0500

    parsing slingshot

--- db/crawlers/parse_slingshot_output.py
+++ db/crawlers/parse_slingshot_output.py
@@ -14,12 +14,24 @@ def geocode(address) :
 def get_contact(listing):
   headline = listing.find('dt').text
   contact_info = listing.find('dd').text
+  if not headline or not contact_info:
+    return
   lat_lng = geocode(contact_info)
-  print(headline, '\n', contact_info, '\n\n')
+  if not lat_lng or len(lat_lng) < 1:
+    return
+  print(headline, '\n', contact_info, '\n', lat_lng,'\n\n')
   return 1+1
 
 def is_defunct(listing):
-  return False
+  if listing.find(class_ = "valid"):
+    ret = False
+
+  elif listing.find(class_ = "defunct"):
+    ret = True
+
+  else:
+    ret = False
+  return ret
 
 for listing in contact_list:
   if is_defunct(listing):
