5742bba4533738c485d3c1e2ae3fd78245357354
mcverter <mitchell.verter@gmail.com>
2017-03-03 08:35:30 -0500

    fixing page objects

--- e2e_tests/nightwatch/pageObjects/create.js
+++ e2e_tests/nightwatch/pageObjects/create.js
@@ -6,28 +6,49 @@ class PageElements {
   constructor({}) {
   }
 }
+const consent_1 = {
+  elements: {}
+};
+
+
+const instructions_2 = {
+  elements: {}
+};
+
+
+const headline_3 = {
+  elements: {}
+};
+
+
+const thing_4 = {
+  elements: {}
+};
+
+
+const space_5 = {
+  elements: {}
+};
+
+const time_6 = {
+  elements: {}
+};
+
+const confirm_7 = {
+  elements: {}
+};
+
 
 module.exports = {
-
     url: () => {return this.api.launch_url + '/shareables/create';},
     steps: {
-      consent_1 : {
-        element: {}
-      },
-      instructions_2 : {
-        element: {}
-      },
-      headline_3 : {
-        element: {}
-      },
-      thing_4 : {
-        element: {}
-      },
-      space_5 : {},
-      time_6 : {},
-      confirm_7: {}
-
-
+      consent_1 : consent_1,
+      instructions_2 : instructions_2,
+      headline_3 : headline_3,
+      thing_4 : thing_4,
+      space_5 : space_5,
+      time_6 : time_6,
+      confirm_7: confirm_7
     }
-  }
-}
+  };
+
