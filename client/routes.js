8921a72905c3eff1ac3e3235ab3d5c9ddc0dff53
mcverter <mitchell.verter@gmail.com>
2017-03-02 21:48:42 -0500

    moving pages

--- client/routes.js
+++ client/routes.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './components/App';
-import FrontPage from './components/FrontPage';
+import FrontPage from './components/pages/FrontPage';
 import AuthRoutes from './routes/auth/routes';
 import DocumentationRoutes from './components/docs/routes.js';
 import ShareableRoutes from './components/pages/routes';
-import WelcomePage from './components/WelcomePage';
+import WelcomePage from './components/pages/WelcomePage';
 
 export default (
   <Route path="/" component={App}>
