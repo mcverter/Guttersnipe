bdca913b3f397cb033161bc1bae58c40e69e2061
mcverter <mitchell.verter@gmail.com>
2017-03-04 10:48:46 -0500

    front page test is working

--- e2e_tests/example.js
+++ e2e_tests/example.js
@@ -1,8 +1,79 @@
+/*
+ module.exports = {
+
+ 'Demo test Google' : function (browser) {
+ browser
+ .url('http://www.google.com')
+ .waitForElementVisible('body', 1000)
+ .setValue('input[type=text]', 'nightwatch')
+ .waitForElementVisible('button[name=btnG]', 1000)
+ .click('button[name=btnG]')
+ .pause(1000)
+ .assert.containsText('#main', 'Night Watch')
+ .end();
+ }
+ };
+ */
+
+/*module.exports = {
+ 'login test': function (client) {
+ client
+ .url('http://localhost:3000')
+ .setValue('input[name=email]', 'foo@bar.com')
+ .click('button[type="submit"]')
+ .assert.containsText('main', 'News feed')
+ .end();
+ }
+ }
+
+ const foo = {
+ url: () => {return this.api.launch_url + 'login'},
+ elements: {
+ emailField :'input[name="email"',
+ passwordField: 'input[name="email"',
+ submitButton: 'button[type="submit"]'
+ }
+ }
+
+ const boo = {
+ 'login test': (client) => {
+ const page = client.page.login();
+
+ page.navigate()
+ .setValue('@emailField', 'foo@bar.com')
+ .setValue('@passwordField', 'passw')
+ .click('@submitButton')
+ .assert.containsText('main', 'News feed');
+
+ client.end();
+ }
+ }
+ */
+
+/*
+
+ src_folders: ['tests'],
+ page_objects_path = 'tests/pages',
+ selenium: SELENIUM_CONFIG,
+ test_steeings: Environment
+
+ }
+
+ */
+
+
+
+
+/*
+
+
+
+
 const webdriver = require('selenium-webdriver');
 const driver = new webdriver.Builder()
   .forBrowser('firefox')
   .build();
-/*
+
 driver.navigate().to('http://localhost:3000/');
 
 
--- e2e_tests/front_page.js
+++ e2e_tests/front_page.js
@@ -3,8 +3,17 @@ module.exports = {
     client
       .url('http://localhost:3000')
       .assert.containsText('body', 'guttersnipe')
+      .assert.elementPresent('.header')
+      .assert.elementPresent('.front-pg')
+      .assert.elementPresent('.btn.btn-lg')
+      .assert.elementPresent('.strummer-quote')
+      .assert.elementPresent('.piaf-img')
+      .assert.elementPresent('.footer')
 
+
+
+      .assert.elementPresent('.kropotkin-quote-outer')
       .end();
   }
+};
 
-}
