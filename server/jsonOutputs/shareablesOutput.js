1fc9cc6ab584e00ab2ea09a264a87ad0e7e8e539
mcverter <mitchell.verter@gmail.com>
2018-06-20 14:04:35 -0400

    backing up old files for new f/e components

---
+++ server/jsonOutputs/shareablesOutput.js
@@ -0,0 +1,245 @@
+const allShareableListItems = [{
+  "id": "96ce431b-89ea-4b93-b79b-ff0c1bae4954",
+  "s_name": "La%20Bagel%20Delight",
+  "s_description": "Bagels%20and%20more%20in%20a%20double-wide%20storefront",
+  "s_address": "%0A%0A%20%20%20%20%20%20%20%20%20%20%20%2090%20Court%20St%2C%20Brooklyn%2C%20NY%2011201",
+  "s_time": "10p"
+},
+  {
+    "id": "d79f0ed6-f5fa-47ac-a99f-e7ea166f8d72",
+    "s_name": "Perelandra",
+    "s_description": "",
+    "s_address": "%20175%20Remsen%20St%20Brooklyn%2C%20NY%2011201%20",
+    "s_time": "8%3A45%20when%20all%20the%20employees%20leave%20the%20store.%20Trash%20is%20collected%20between%2010%3A30%20p%20and%2012%3A30a.%20One%20source%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20says%20nothing%20is%20out%20on%20Saturdays."
+  },
+  {
+    "id": "95d70cb2-afdf-4242-b44b-c92bec235cd4",
+    "s_name": "Garden%20of%20Eden",
+    "s_description": "%20grocery",
+    "s_address": "%20180%20Montague%20St%20%231%20Brooklyn%2C%20NY%2011201",
+    "s_time": "Sunday-Friday%2010p"
+  },
+  {
+    "id": "2bba049d-d4d5-4ff3-b885-254d84308257",
+    "s_name": "Key%20Foods",
+    "s_description": "Grocery%20store",
+    "s_address": "%0A%20%20%20%20%20%20%20%20%20%20%20%20169%20Atlantic%20Ave%2C%20Brooklyn%2C%20NY%2011201",
+    "s_time": ""
+  },
+  {
+    "id": "d6c98b50-ada1-43ef-8899-653e079b94a8",
+    "s_name": "Bagel%20World",
+    "s_description": "",
+    "s_address": "181%20Court%20St%2C%20Brooklyn%2C%20NY%2011201",
+    "s_time": ""
+  },
+  {
+    "id": "4e81ae91-a0ed-41db-8c43-292ee64087f4",
+    "s_name": "Kim%27s%20Millenium%20Market",
+    "s_description": "",
+    "s_address": "%0A%20%20%20%20%20%20%20%20%20%20%20%20260%20Bushwick%20Ave%2C%20Brooklyn%2C%20NY%2011206",
+    "s_time": ""
+  },
+  {
+    "id": "edf4197a-84e8-4d24-9c70-44dd6bb5f425",
+    "s_name": "Gristedes",
+    "s_description": "Supermarket",
+    "s_address": "%20101%20Clark%20St%20Brooklyn%2C%20NY%2011201",
+    "s_time": ""
+  },
+  {
+    "id": "2aaf91ae-62ce-47dd-9aa2-3f86801c8b7d",
+    "s_name": "Trader%20Joe%27s",
+    "s_description": "",
+    "s_address": "Atlantic%20at%20Court%20130%20Court%20St%20Brooklyn%2C%20NY%2011201",
+    "s_time": "Lately%20%28fall%202012%29%2C%20usually%20not%20until%20about%20midnight%3B%20sometimes%20earlier.%20Be%20forewarned%2C%20the%20initial%0A%20%20%20%20%20%20%20%20%20%20%20%20reaction%20of%20this%20store%27s%20management%20was%20to%20have%20the%20police%20ticket%20dumpster%20divers%20for%20trespass%20or%20littering%3B%0A%20%20%20%20%20%20%20%20%20%20%20%20the%20situation%20could%20always%20take%20a%20turn%20for%20the%20worse."
+  },
+  {
+    "id": "d15d9c76-7e10-4013-9438-c4c9283cedfa",
+    "s_name": "Union%20Market",
+    "s_description": "",
+    "s_address": "%20288%20Court%20St%20Brooklyn%2C%20NY%2011231",
+    "s_time": ""
+  },
+  {
+    "id": "c3c234d4-be20-4215-a39d-2e09c522e25a",
+    "s_name": "Associated%20Supermarket",
+    "s_description": "",
+    "s_address": "%201413%20Avenue%20J%20Brooklyn%2C%20NY%2011230",
+    "s_time": ""
+  },
+  {
+    "id": "3ea7ad07-3e46-4b74-8b14-ad2b3cbfb7af",
+    "s_name": "Caputo%27s%20Bakery",
+    "s_description": "Italian%20bakery",
+    "s_address": "329%20Court%20Street%2C%20brooklyn%2C%20ny",
+    "s_time": ""
+  },
+  {
+    "id": "6a6b1764-5506-4e4f-9647-7d5dfcfa2e54",
+    "s_name": "New%20Banana%20King",
+    "s_description": "",
+    "s_address": "%201123%20Avenue%20J%20Brooklyn%2C%20NY%2011230",
+    "s_time": ""
+  },
+  {
+    "id": "25c79cc3-1987-4f32-888b-d2b12d34846c",
+    "s_name": "Isaac%27s%20bake%20shop",
+    "s_description": "",
+    "s_address": "%201419%20Avenue%20J%20Brooklyn%2C%20NY%2011230",
+    "s_time": ""
+  },
+  {
+    "id": "b7294282-78ce-4d39-af45-125bb3a69e9e",
+    "s_name": "Brooklyn%20Fare",
+    "s_description": "",
+    "s_address": "200%20Schermerhorn%20Street%20Brooklyn%2C%20NY%2011201",
+    "s_time": ""
+  },
+  {
+    "id": "06b8c8ed-ca51-421f-87ce-3fde66c48118",
+    "s_name": "Union%20Market",
+    "s_description": "",
+    "s_address": "754%20Union%20St%2C%20Brooklyn%2C%20NY%2011215%2C%20USA",
+    "s_time": ""
+  },
+  {
+    "id": "56ce136a-3a7b-4891-890b-f12f9eb29861",
+    "s_name": "D%27Vine%20Taste",
+    "s_description": "",
+    "s_address": "150%207th%20Ave%2C%20Brooklyn%2C%20NY%2011215",
+    "s_time": ""
+  },
+  {
+    "id": "6c8c123a-6950-4bcf-9307-e7acaf4e9b26",
+    "s_name": "Key%20Foods",
+    "s_description": "Supermarket",
+    "s_address": "130%207th%20Avenue%2C%20brooklyn%20ny",
+    "s_time": ""
+  },
+  {
+    "id": "3f6f3822-41f4-4c3d-8969-8fb88b0075bc",
+    "s_name": "Sunac%20Natural%20Foods",
+    "s_description": "",
+    "s_address": "150%20N%207th%20St%2C%20Brooklyn%2C%20NY%2011249",
+    "s_time": ""
+  },
+  {
+    "id": "6935cad1-a4c9-4f5c-bf3a-9f6c990f567d",
+    "s_name": "South%20Side%20Food%20Plaza",
+    "s_description": "%20kosher%20supermarket",
+    "s_address": "%20428%20Bedford%20Ave%2C%20Brooklyn%2C%20NY%2011249",
+    "s_time": ""
+  },
+  {
+    "id": "ea5932f0-ddc5-4f4c-9406-62d2528bc133",
+    "s_name": "Food%20Bazaar",
+    "s_description": "",
+    "s_address": "%0A%20%20%20%20%20%20%20%20%20%20%20%2021%20Manhattan%20Ave%2C%20Brooklyn%2C%20NY%2011206",
+    "s_time": ""
+  },
+  {
+    "id": "57efecf4-28cd-4956-8af6-be5b09334a99",
+    "s_name": "Depanneur",
+    "s_description": "",
+    "s_address": "%0A%20%20%20%20%20%20%20%20%20%20%20%20242%20Wythe%20Ave%2C%20Brooklyn%2C%20NY%2011249",
+    "s_time": ""
+  },
+  {
+    "id": "3da77a3c-1ffd-4c68-a9c9-8e2d84e3b552",
+    "s_name": "La%20Bagel%20Delight",
+    "s_description": "Bagels%20and%20more%20in%20a%20double-wide%20storefront",
+    "s_address": "90%20Court%20St%2C%20Brooklyn%2C%20NY%2011201",
+    "s_time": "10p"
+  },
+  {
+    "id": "d984fd84-56eb-4dd1-be58-15edaeba438b",
+    "s_name": "Trader%20Joe%27s",
+    "s_description": "",
+    "s_address": "Atlantic%20at%20Court%20130%20Court%20St%20Brooklyn%2C%20NY%2011201",
+    "s_time": "Lately%20%28fall%202012%29%2C%20usually%20not%20until%20about%20midnight%3B%20sometimes%20earlier.%20Be%20forewarned%2C%20the%20initial%20reaction%20of%20this%20store%27s%20management%20was%20to%20have%20the%20police%20ticket%20dumpster%20divers%20for%20trespass%20or%20littering%3B%20the%20situation%20could%20always%20take%20a%20turn%20for%20the%20worse."
+  },
+  {
+    "id": "36142fdc-0c08-4889-bba3-b60b943c2a95",
+    "s_name": "Perelandra",
+    "s_description": "",
+    "s_address": "175%20Remsen%20St%20Brooklyn%2C%20NY%2011201",
+    "s_time": "8%3A45%20when%20all%20the%20employees%20leave%20the%20store.%20Trash%20is%20collected%20between%2010%3A30%20p%20and%2012%3A30a.%20One%20source%20says%20nothing%20is%20out%20on%20Saturdays."
+  },
+  {
+    "id": "7c5a7e66-e4fd-4c95-b199-06f706904ea9",
+    "s_name": "Associated%20Supermarket",
+    "s_description": "",
+    "s_address": "1413%20Avenue%20J%20Brooklyn%2C%20NY%2011230",
+    "s_time": ""
+  },
+  {
+    "id": "af70e494-7844-4ddb-8b5b-61ff66e5e55e",
+    "s_name": "Garden%20of%20Eden",
+    "s_description": "grocery",
+    "s_address": "180%20Montague%20St%20%231%20Brooklyn%2C%20NY%2011201",
+    "s_time": "Sunday-Friday%2010p"
+  },
+  {
+    "id": "41ae831e-ff57-464f-b62d-57d35cef4648",
+    "s_name": "Gristedes",
+    "s_description": "Supermarket",
+    "s_address": "101%20Clark%20St%20Brooklyn%2C%20NY%2011201",
+    "s_time": ""
+  },
+  {
+    "id": "e1ff0bf8-a94b-4540-93ea-81905090f6dd",
+    "s_name": "Key%20Foods",
+    "s_description": "Grocery%20store",
+    "s_address": "169%20Atlantic%20Ave%2C%20Brooklyn%2C%20NY%2011201",
+    "s_time": ""
+  },
+  {
+    "id": "fcdda06e-6574-4927-923e-bb12158a6d8c",
+    "s_name": "New%20Banana%20King",
+    "s_description": "",
+    "s_address": "1123%20Avenue%20J%20Brooklyn%2C%20NY%2011230",
+    "s_time": ""
+  },
+  {
+    "id": "0a504ba7-2a1f-41a5-ab01-1188676987ba",
+    "s_name": "Isaac%27s%20bake%20shop",
+    "s_description": "",
+    "s_address": "1419%20Avenue%20J%20Brooklyn%2C%20NY%2011230",
+    "s_time": ""
+  },
+  {
+    "id": "6959e8eb-7d0f-4038-986a-09381eedd454",
+    "s_name": "Kim%27s%20Millenium%20Market",
+    "s_description": "",
+    "s_address": "260%20Bushwick%20Ave%2C%20Brooklyn%2C%20NY%2011206",
+    "s_time": ""
+  },
+  {
+    "id": "208a9259-8c1f-41f0-93f2-1a92583856f5",
+    "s_name": "Union%20Market",
+    "s_description": "",
+    "s_address": "288%20Court%20St%20Brooklyn%2C%20NY%2011231",
+    "s_time": ""
+  },
+  {
+    "id": "2f5b0b2e-900d-4ca2-a335-45bae61734ef",
+    "s_name": "South%20Side%20Food%20Plaza",
+    "s_description": "kosher%20supermarket",
+    "s_address": "428%20Bedford%20Ave%2C%20Brooklyn%2C%20NY%2011249",
+    "s_time": ""
+  },
+  {
+    "id": "5ea7c3e5-f137-4047-b6c2-1a7b2556512c",
+    "s_name": "Food%20Bazaar",
+    "s_description": "",
+    "s_address": "21%20Manhattan%20Ave%2C%20Brooklyn%2C%20NY%2011206",
+    "s_time": ""
+  },
+  {
+    "id": "cb1bda4b-643b-434a-a435-096db1dbd584",
+    "s_name": "Depanneur",
+    "s_description": "",
+    "s_address": "242%20Wythe%20Ave%2C%20Brooklyn%2C%20NY%2011249",
+    "s_time": ""
+  }]
