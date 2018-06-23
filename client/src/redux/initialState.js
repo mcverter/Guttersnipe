17a91f37c2250e0781fb476da860a9a3df7bfa14
mcverter <mitchell.verter@gmail.com>
2018-06-23 15:02:50 -0400

    adding redux skeletons

--- client/src/redux/initialState.js
+++ client/src/redux/initialState.js
@@ -2,22 +2,49 @@ import commentsJSON from '../../../db/data/json/commentsOutput';
 import shareablesJSON from '../../../db/data/json/shareablesOutput';
 
 const initialState = {
-  shareables : shareablesJSON,
-  auth : {
-    authenticated: false,
-    error: '',
-    username: ''
+  shareables : {
+    isFetchingShareables: false,
+    shareableFetchError: false,
+    shareables: shareablesJSON,
+    selectedIndex: -1,
+  },
+  comments : {
+    comments : commentsJSON,
+    isFetchingComments: false,
+    shareableCommentsError: false,
+  },
+  categories : {
+    categories: undefined,
+    isFetchingCategories: false,
+    categoriesFetchError: false,
+  },
+  subcategories : {
+    subcategories: undefined,
+    isFetchingSubcategories: false,
+    subcategoriesFetchError: false,
   },
   kropotkin: {
     isFetchingKropotkin: false,
     kropotkinFetchError: false,
     paragraph: ''
   },
+/* Not implemented yet */
+  auth : {
+    authenticated: false,
+    error: '',
+    username: ''
+  },
   browserEnv: {
     location: [18.135228, -97.0901879],
     latitude: 18.135228,
     longitude: -97.0901879
+  },
+  mapCenter: {
+    location: [18.135228, -97.0901879],
+    latitude: 18.135228,
+    longitude: -97.0901879
   }
+
 };
 
 export default initialState;
