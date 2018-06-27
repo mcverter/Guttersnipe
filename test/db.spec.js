727f525ba7a0622e2211b37683eeee3831c9272f
mcverter <mitchell.verter@gmail.com>
2018-06-27 17:17:45 -0400

    tests working (2 not passing)

--- test/db.spec.js
+++ test/db.spec.js
@@ -1,6 +1,15 @@
 var expect = require('chai').expect; //actually call the function
 const {client} = require("./helpers/dbHelper");
 
+function printError(error) {
+  console.error("SELECT ERROR", error);
+  expect(1).to.equal(0);
+}
+
+function printResults (results) {
+  console.info("SELECT RESULTS", results);
+}
+
 class UUID {
   constructor() {
     this._uuid = null;
@@ -14,159 +23,188 @@ class UUID {
   }
 }
 
-
 let gristedesId = new UUID(); //
 
 describe('Database', function () {
   it('is populated', function (done) {
     client.query("SELECT * FROM shareable")
       .then(results => {
-        console.info("SELECT RESULTS", results);
+        printResults(results);
         expect(results.rows.length).to.be.above(0);
         done()
       })
-      .catch(error=>{console.error("SELECT ERROR", error);})
+      .catch(error=>{
+        printError(error);
+      })
   });
 
   describe('Gristedes', function () {
-    it('is in DB', function () {
+    it('is in DB', function (done) {
       client.query("SELECT * FROM shareable WHERE name='Gristedes'")
         .then(results => {
-          console.info("SELECT RESULTS", results);
+          printResults(results);
           gristedesId = results.rows[0].id;
           console.log('Gristedes ID', gristedesId);
           expect(results.rows.length).to.be.above(0);
           done()
         })
         .catch(error=>{
-          console.error("SELECT ERROR", error);
-          expect(1).to.equal(0);
+          printError(error);
         })
     });
-    it('has a subcategory', function () {
+    it('has a subcategory', function (done) {
       try {
 //        let query =
         client.query(`SELECT subcategory FROM shareable WHERE id='${gristedesId}'`)
           .then(results => {
-            console.info("SELECT RESULTS", results);
+            printResults(results);
             expect(results.rows.length).to.be.above(0);
             done()
           })
-          .catch(error=>{console.error("SELECT ERROR", error);})
+          .catch(error=>{
+            printError(error);
+          })
       }
       catch (error) {
         console.error(console.error("SELECT ERROR", error));
+        expect(1).to.equal(0);
       }
-     });
-    it('has a category', function () {
-      client.query(`SELECT category FROM shareable WHERE id='${gristedesId}'`)
+    });
+    it('has a category', function (done) {
+      client.query(`
+        SELECT category 
+        FROM category_subcategory 
+         JOIN shareable 
+         ON shareable.subcategory = category_subcategory.subcategory
+         AND shareable.id ='${gristedesId}'`)
         .then(results => {
-          console.info("SELECT RESULTS", results);
+          printResults(results);
           expect(results.rows.length).to.be.above(0);
           done()
         })
-        .catch(error=>{console.error("SELECT ERROR", error);})
+        .catch(error=>{
+          printError(error);
+        })
 
     });
-    it('has a name', function () {
+    it('has a name', function (done) {
       client.query(`SELECT name FROM shareable WHERE id='${gristedesId}'`)
         .then(results => {
-          console.info("SELECT RESULTS", results);
+          printResults(results);
           expect(results.rows.length).to.be.above(0);
           done()
         })
-        .catch(error=>{console.error("SELECT ERROR", error);})
+        .catch(error=>{
+          printError(error);
+        })
 
     });
-    it('may have a description', function () {
+    it('may have a description', function (done) {
       client.query(`SELECT description FROM shareable WHERE id='${gristedesId}'`)
         .then(results => {
-          console.info("SELECT RESULTS", results);
+          printResults(results);
           // expect(results.rows.length).to.be.above(0);
           done()
         })
-        .catch(error=>{console.error("SELECT ERROR", error);})
+        .catch(error=>{
+          printError(error);
+        })
     });
-    it('has an address', function () {
+    it('has an address', function (done) {
       client.query(`SELECT address FROM shareable WHERE id='${gristedesId}'`)
         .then(results => {
-          console.info("SELECT RESULTS", results);
+          printResults(results);
           expect(results.rows.length).to.be.above(0);
           done()
         })
-        .catch(error=>{console.error("SELECT ERROR", error);})
+        .catch(error=>{
+          printError(error);
+        })
     });
-    it('has a geolocation', function () {
+
+    it('has a geolocation', function (done) {
       client.query(`SELECT geolocation FROM shareable WHERE id='${gristedesId}'`)
         .then(results => {
-          console.info("SELECT RESULTS", results);
+          printResults(results);
           expect(results.rows.length).to.be.above(0);
           done()
         })
-        .catch(error=>{console.error("SELECT ERROR", error);})
+        .catch(error=>{
+          printError(error);
+        })
 
     });
-    it('may have a time', function () {
+    it('may have a time', function (done) {
       client.query(`SELECT time FROM shareable WHERE id='${gristedesId}'`)
         .then(results => {
-          console.info("SELECT RESULTS", results);
+          printResults(results);
           // expect(results.rows.length).to.be.above(0);
           done()
         })
-        .catch(error=>{console.error("SELECT ERROR", error);})
+        .catch(error=>{
+          printError(error);
+        })
     });
-    it('may have an icalendar', function () {
-      client.query(`SELECT ical FROM shareable WHERE id='${gristedesId}'`)
+    it('may have an icalendar', function (done) {
+      client.query(`SELECT icalendar FROM shareable WHERE id='${gristedesId}'`)
         .then(results => {
-          console.info("SELECT RESULTS", results);
+          printResults(results);
           // expect(results.rows.length).to.be.above(0);
           done()
         })
-        .catch(error=>{console.error("SELECT ERROR", error);})
+        .catch(error=>{
+          printError(error);
+        })
     });
-    it('has three comments', function () {
-      client.query(`SELECT comments FROM shareable WHERE id='${gristedesId}'`)
+    it('has three comments', function (done) {
+      client.query(`
+      SELECT * FROM shareable_comment
+       JOIN shareable 
+       ON shareable.id = shareable_comment.shareable_id 
+       AND shareable.id='${gristedesId}'`)
         .then(results => {
-          console.info("SELECT RESULTS", results);
+          printResults(results);
           expect(results.rows.length).to.equal(3);
           done()
         })
-        .catch(error=>{console.error("SELECT ERROR", error);})
+        .catch(error=>{
+          printError(error);
+        })
     });
   });
-  describe('User', function () {
-    it('has a name', function () {
+  /*
+  describe('User', function (done) {
+    it('has a name', function (done) {
     });
-    it('may have an email', function () {
+    it('may have an email', function (done) {
     });
-    it('may have an expiration', function () {
+    it('may have an expiration', function (done) {
     });
-    it('may have an role', function () {
-    });
-  });
-  describe('Comments', function () {
-    it('has a title', function () {
-    });
-    it('has a text', function () {
-    });
-    it('has a posting date', function () {
-    });
-    it('references a Shareable', function () {
-    });
-    it('references a User', function () {
+    it('may have an role', function (done) {
     });
   });
-  describe('Kropotkin', function () {
-    it('has a paragraph', function () {
+  describe('Comments', function (done) {
+    it('has a title', function (done) {
+    });
+    it('has a text', function (done) {
+    });
+    it('has a posting date', function (done) {
+    });
+    it('references a Shareable', function (done) {
+    });
+    it('references a User', function (done) {
     });
   });
-  describe('CategorySubcategory', function () {
-    it('can retrieve a category for a given subcategory', function () {
+  describe('Kropotkin', function (done) {
+    it('has a paragraph', function (done) {
     });
-    it('can retrieve a list of subcategories for a given category', function () {
+  });
+  describe('CategorySubcategory', function (done) {
+    it('can retrieve a category for a given subcategory', function (done) {
     });
-    it('matches all categories with subcategories and vica versa', function () {
+    it('can retrieve a list of subcategories for a given category', function (done) {
+    });
+    it('matches all categories with subcategories and vica versa', function (done) {
     })
-
-  })
+  })*/
 });
