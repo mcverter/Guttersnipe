89e0276f1cb9ed945c66949f39525e5c49938c78
mcverter <mitchell.verter@gmail.com>
2018-06-28 10:25:38 -0400

    simplifying old redux files for reuse

---
+++ client/src/redux/actions/category.js
@@ -0,0 +1,23 @@
+
+
+export function fetchShareableCategorizations() {
+  return (dispatch, getState) => {
+    const shareableState = getState().shareables;
+    if (!shareableState.categorizationMeta ||
+      !shareableState.categorizationMeta.types) {
+      dispatch({type: types.SHAREABLE_CATEGORIZATION_REQUEST});
+      return fetch(`${SERVER_URL}/api/shareables/categorization`)
+        .then(response => response.json())
+        .then(json => dispatch(receiveShareableCategorization(json)));
+    }
+  };
+}
+
+function receiveShareableCategorization(json) {
+  return {
+    type: types.SHAREABLE_CATEGORIZATIONS_SUCCESS,
+    categorizationMeta: json
+  };
+}
+
+
--- client/src/redux/actions/shareable.js
+++ client/src/redux/actions/shareable.js
@@ -1,21 +1,12 @@
 import * as types from "../types";
-import _ from "lodash";
+import find from "lodash.find";
 import {SERVER_URL} from "../../config";
-// import {browserHistory} from 'react-router';
 
-export function fetchAllShareablesIfNeeded(forceFetch=false, page_num=1) {
-  return (dispatch, getState) => {
-    const state = getState();
-    if (forceFetch || ! state.shareables.items || state.shareables.items.length <= 0) {
-      return dispatch(searchShareablesWithParametersAndPagination({page_num: 1}));
-    }
-  };
-}
 
-export function fetchAllShareables(dispatch, page_num) {
+export function fetchAllShareables(dispatch) {
   return () => {
     dispatch({type: types.SHAREABLES_ALL_REQUEST});
-    return fetch(`${SERVER_URL}/api/shareables?page_num=${page_num}`)
+    return fetch(`${SERVER_URL}/ shareables`)
       .then(response => response.json())
       .then(json => dispatch(receiveAllShareables(json)));
   };
@@ -39,7 +30,7 @@ function setCurrentShareable(id) {
 
 function shouldFetchSingleShareable(state, id) {
   return !(state.shareables || !state.shareables.items ||
-    _.find(state.shareables.items, {id: parseInt(id)}));
+    find(state.shareables.items, {id: parseInt(id)}));
 }
 
 
@@ -57,41 +48,12 @@ function fetchSingleShareable(id) {
   return function (dispatch) {
     dispatch(requestSingleShareable(id));
 
-    return fetch(`${SERVER_URL}/api/shareable/${id}`)
+    return fetch(`${SERVER_URL}/shareable/${id}`)
       .then(response=>response.json())
       .then(json=>
         dispatch(receiveSingleShareable(json)));
   };
 }
-
-
-
-export function createShareable(data) {
-  fetch(`${SERVER_URL}/api/shareables`, { method: 'POST', body: JSON.stringify(data) });
-}
-
-
-export function fetchShareableCategorizations() {
-  return (dispatch, getState) => {
-    const shareableState = getState().shareables;
-    if (!shareableState.categorizationMeta ||
-      !shareableState.categorizationMeta.types) {
-      dispatch({type: types.SHAREABLE_CATEGORIZATION_REQUEST});
-      return fetch(`${SERVER_URL}/api/shareables/categorization`)
-        .then(response => response.json())
-        .then(json => dispatch(receiveShareableCategorization(json)));
-    }
-  };
-}
-
-function receiveShareableCategorization(json) {
-  return {
-    type: types.SHAREABLE_CATEGORIZATIONS_SUCCESS,
-    categorizationMeta: json
-  };
-}
-
-
 function requestSingleShareable() {
   return {
     type: types.SHAREABLE_SINGLE_REQUEST
@@ -104,38 +66,4 @@ function receiveSingleShareable(json) {
     shareables: json
   };
 }
-function shareableSearchRequest() {
-  return {
-    type: types.SHAREABLES_SEARCH_REQUEST
-  };
-}
 
-function saveSearchParams(searchParams) {
-  return {
-    type: types.SAVE_SEARCH_PARAMS,
-    searchParams: searchParams
-  };
-}
-export function searchShareablesWithParametersAndPagination(options){
-
-  let {forceFetch, page_num, pageSize, searchParams}=options;
-  forceFetch = forceFetch || false;
-  page_num = page_num || 1;
-  pageSize = pageSize || 20;
-  searchParams = searchParams || {};
-  return dispatch => {
-    dispatch(shareableSearchRequest());
-    dispatch(saveSearchParams(searchParams));
-    return fetch(`${SERVER_URL}/api/shareables/search`,
-      { method: 'POST', body: JSON.stringify({
-        search_params:searchParams,
-        page_num: page_num,
-        page_size: pageSize
-      }) })
-      .then(response=>response.json())
-      .then(json=> {
-        dispatch(receiveAllShareables(json));
-   //     browserHistory.push('/shareables');
-      });
-  };
-}
