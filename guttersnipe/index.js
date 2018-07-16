a4bf4062ec7dcee89b28ecce1e56f4d569b5dc6e
mcverter <mitchell.verter@gmail.com>
2018-07-15 21:18:40 -0400

    repress react-native warning

--- guttersnipe/index.js
+++ guttersnipe/index.js
@@ -1,3 +1,7 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
+
+import { YellowBox } from 'react-native';
+YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
+
 AppRegistry.registerComponent('guttersnipe', () => App);
