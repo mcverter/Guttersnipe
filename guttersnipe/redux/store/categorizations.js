720c33ef534dd4a05c3bd94fb0e54ae9ddb78da6
mcverter <mitchell.verter@gmail.com>
2018-07-17 09:27:37 -0400

    will use a big JSON blob rather than redux store

--- guttersnipe/redux/store/categorizations.js
+++ guttersnipe/redux/store/categorizations.js
@@ -1,5 +1,5 @@
 const categorizations = {
-  'food': ['dumpster', 'food not bombs', 'free meal', 'food pantry'],
+  'food': ['dumpster', 'food not bombs', 'pantry/kitchen'],
   'medical': ['needle exchange', 'free clinic (western)', "free clinic (eastern)"],
   'shelter': ['squat', 'abandoned building', 'infoshop']
 };
