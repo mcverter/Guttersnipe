cc79ce3a9ae0bd8177e1a8ab8c84a00d23325b03
mcverter <mitchell.verter@gmail.com>
2018-11-19 21:40:04 -0500

    adding build folders

---
+++ client/web/build/precache-manifest.008bae0926f1e6b522fab7f4e71458ea.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b976f244ee765fd2cc4aad1c84bbfe33",
+    "url": "/static/media/roadrunnerAtWaste.b976f244.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2466d2562873f7cbb58ae3e9151ffc23",
+    "url": "/static/media/Guttersnipe.2466d256.mp4"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "23fea0b5d71acc892404",
+    "url": "/static/js/main.23fea0b5.chunk.js"
+  },
+  {
+    "revision": "b59a99554d969440c184",
+    "url": "/static/js/1.b59a9955.chunk.js"
+  },
+  {
+    "revision": "23fea0b5d71acc892404",
+    "url": "/static/css/main.9ac701d6.chunk.css"
+  },
+  {
+    "revision": "29304ffcd21cce204fc1381d12b95b4a",
+    "url": "/index.html"
+  }
+];
