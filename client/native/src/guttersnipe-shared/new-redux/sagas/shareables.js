ccc304c1726644229f73a6f2d40280d116dc1d57
mcverter <mitchell.verter@gmail.com>
2019-01-03 17:29:30 -0500

    building sagas

--- client/native/src/guttersnipe-shared/new-redux/sagas/shareables.js
+++ client/native/src/guttersnipe-shared/new-redux/sagas/shareables.js
@@ -1,9 +1,31 @@
-import {take, put} from 'redux-saga/efects';
-import * as types from '../types';
+import {takeEvery,delay} from 'redux-saga'
+import {call, put, take} from 'redux-saga/effects'
+import {asyncTestInitial,asyncTestSaga,asyncFetchInitial,
+  asyncFetchSuccess,asyncFetchError} from '../actions/middlewareActions'
 
-export function* shareablesFetchSaga(){
-  while (true){
-    yield take()
 
+export function ShareableFetchAll(){
+  return fetch('testdata.json')
+    .then((res)=> res.json())
+    .then((jsondata)=> {
+      const {dummyOutput} = jsondata.dummy
+      return dummyOutput
+    })
+}
+
+export  function* shareableFetchSaga (action){
+  yield call(delay,1000)
+  yield put(asyncFetchInitial())
+  try {
+    const dummyOutput = yield call(FetchTestData)
+    yield put(asyncFetchSuccess(dummyOutput))
+  } catch (error) {
+    console.log("Error in fetch" + error)
+    yield put(asyncFetchError())
   }
+}
+
+
+export default function* shareableFetchWatcher (){
+  yield takeEvery("ASYNC_TEST_SAGA",fetchAsyncSaga)
 }
