97c7e79c3f00d7aaedeb91fb6584eded22c4f5c3
mcverter <mitchell.verter@gmail.com>
2019-01-04 22:08:47 -0500

    cors

--- src/server/app.js
+++ src/server/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require('cors')
 
 const indexRouter = require("./routes/index");
 const kropotkinRouter = require("./routes/kropotkins");
@@ -10,6 +11,7 @@ const shareableRouter = require("./routes/shareables");
 
 const app = express();
 
+app.use(cors())
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
